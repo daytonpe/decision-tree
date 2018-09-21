c5835c27202af483f8bb55ce3e5bb9afacb2efff
Pat Dayton <daytonpe@Pats-MacBook-Pro.local>
2018-09-21 10:01:16 -0500

    basic functionality complete

--- driver.py
+++ driver.py
@@ -1,12 +1,19 @@
 from DecisionTree import *
 import pandas as pd
 from sklearn import model_selection
-import math
 
 header = ['SepalL', 'SepalW', 'PetalL', 'PetalW', 'Class']
+
+# Iris Data
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
                  header=None,
                  names=['SepalL', 'SepalW', 'PetalL', 'PetalW', 'Class'])
+
+# Tic Tac Toe Data
+# df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.data',
+#                  header=None,
+#                  names=['TL', 'TM', 'TR', 'ML', 'MM', 'MR', 'BL', 'BM', 'BR', 'Class'])
+
 lst = df.values.tolist()
 # print(class_counts(lst))  # {'Iris-versicolor': 50, 'Iris-setosa': 50, 'Iris-virginica': 50}
 # print(entropy(lst))  # 1.58 which is max entropy, i.e. log2(k) where k is number of categories
@@ -14,35 +21,33 @@ lst = df.values.tolist()
 # print(entropy(lst[10:65]))  # .84
 # print(entropy(lst[0:2]))  # 0.0 min entropy (all the same flower classification)
 t = build_tree(lst, header)
+print("\n\n********** Decision Tree ****************\n")
 print_tree(t)
 
-print("\n\n********** Leaf nodes ****************")
+print("\n\n********** Leaf nodes ****************\n")
 leaves = getLeafNodes(t)
 for leaf in leaves:
     print("id = " + str(leaf.id) + " depth =" + str(leaf.depth))
 
-print("\n\n********** Non-leaf nodes ****************")
+print("\n\n********** Non-leaf nodes ****************\n")
 innerNodes = getInnerNodes(t)
 for inner in innerNodes:
     print("id = " + str(inner.id) + " depth =" + str(inner.depth))
 
-print('\n\n')
-print(classify(lst[50], t))
+trainDF, testDF = model_selection.train_test_split(df, test_size=0.2)
+train = trainDF.values.tolist()
+test = testDF.values.tolist()
 
-# trainDF, testDF = model_selection.train_test_split(df, test_size=0.2)
-# train = trainDF.values.tolist()
-# test = testDF.values.tolist()
-#
-# t = build_tree(train, header)
-# print("*************Tree before pruning*******")
-# print_tree(t)
-# acc = computeAccuracy(test, t)
-# print("Accuracy on test = " + str(acc))
-#
+t = build_tree(train, header)
+print("\n\n*************Tree before pruning*******\n")
+print_tree(t)
+acc = computeAccuracy(test, t)
+print("\nAccuracy on test = " + str(acc))
+
 # ## TODO: You have to decide on a pruning strategy
-# t_pruned = prune_tree(t, [26, 11, 5])
-#
-# print("*************Tree after pruning*******")
-# print_tree(t_pruned)
-# acc = computeAccuracy(test, t)
-# print("Accuracy on test = " + str(acc))
+t_pruned = prune_tree(t, [26, 11, 5])
+
+print("\n\n*************Tree after pruning*******\n")
+print_tree(t_pruned)
+acc = computeAccuracy(test, t)
+print("\nAccuracy on test = " + str(acc))
